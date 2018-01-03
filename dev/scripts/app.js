b5f59978bf2d411714d95ac81286ad84a346179b
nguyenasaurus <justinexnguyen@gmail.com>
2018-01-03 14:16:54 -0500

    added in start and how to play button and added fonts + variables

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import Board from './board'
-
-import levels from './levels'
+import Instructions from './instructions';
+import Board from './board';
+import levels from './levels';
 
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
+      showInstructions: true,
+      showBoard: false,
       currentLevel: [],
       score: 0
     }
+    this.handleInstructionClick = this.handleInstructionClick.bind(this);
+    this.handlePlayClick = this.handlePlayClick.bind(this);
     this.playLevel = this.playLevel.bind(this);
-    this.updateScore = this.updateScore.bind(this)
+    this.updateScore = this.updateScore.bind(this);
+  }
+
+  handleInstructionClick() {
+    this.setState(prevState => ({
+      showInstructions: !prevState.showInstructions
+    }));
+  }
+
+  handlePlayClick() {
+
   }
 
   playLevel(i) {
@@ -35,12 +48,33 @@ class App extends React.Component {
 
     render() {
       return (
-        <Board
-          score={this.state.score}
-          updateScore={this.updateScore}
-          level={this.state.currentLevel} />
+
+        // conditional rendering based on button clicks
+        // button click play display board
+        // button click how to play display Instructions
+          
+        <div className="gameFrame">
+          
+          <button onClick={this.handlePlayClick}>
+            {this.state.showBoard ? 'Play' : 'Close'}
+          </button>
+          
+          <button onClick={this.handleInstructionClick}>
+            {this.state.showInstructions ? 'How to Play' : 'Close'}
+          </button>
+            <Instructions instructions={this.state.showInstructions} />
+          <Board
+            score={this.state.score}
+            updateScore={this.updateScore}
+            level={this.state.currentLevel} />
+
+        </div>
+
+          
       )
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+

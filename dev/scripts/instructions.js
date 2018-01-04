d9fcb43d80680bb2c19dab171cb74f00ac24d763
nguyenasaurus <justinexnguyen@gmail.com>
2018-01-04 13:19:42 -0500

    intro component and conditional component render for board

--- dev/scripts/instructions.js
+++ dev/scripts/instructions.js
@@ -1,38 +1,38 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-function Instructions(props) {
-    if (!props.instructions) {
-        return null
+export default class Instructions extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
     }
-    return 
-    (
-        <div className="instructions">
 
-            <h2>How to Play</h2>
-            <p>
-                Help Stu get out of the sewers, through the garden and into the kitchen pantry.
-            </p>
-            <ul>
-                <li>Use your arrow keys to move Stu around the maze to the finish line.
+    render() {
+        return (
+            <div className="instructions">
+
+                <h2>How to Play</h2>
+                <ul>
+                    <li>Use your arrow keys to move Stu around the maze to the finish line.
                 <span className="arrows"></span>
-                </li>
-                <li>You can pick up crumbs and cheese on your way...
+                    </li>
+                    <li>You can pick up crumbs and cheese on your way...
                 <span className="crumbs"></span>
-                    <span className="cheese"></span>
-                </li>
-                <li>But avoid all tricky traps and fierce felines on the prowl.
+                        <span className="cheese"></span>
+                    </li>
+                    <li>But avoid all tricky traps and fierce felines on the prowl.
                     <span className="trap"></span>
-                    <span className="cat"></span>
-                </li>
-                <li>There are also bridges you may have to cross to get to the finish line. Just hit the switch to change the bridge direction.
+                        <span className="cat"></span>
+                    </li>
+                    <li>There are also bridges you may have to cross to get to the finish line. Just hit the switch to change the bridge direction.
                 <span class="switch"></span>
-                    <span class="bridge"></span>
-                </li>
-            </ul>
+                        <span class="bridge"></span>
+                    </li>
+                </ul>
 
-        </div>
-    )
+            </div>
+        )
+    }
 }
-
-
-export default Instructions
